5d265970dd39fbfac290a6e0775caf239778bcec
KEXUJIAN <kexujian@163.com>
2019-11-26 02:34:32 +0800

    commit

--- demo/src/client/oom.ts
+++
@@ -1,9 +0,0 @@
-import { Tool } from "../util/Tool";
-import { readFileSync } from "fs";
-import { join } from "path";
-import { ROOT } from "../entity/BaseEntity";
-for (let i = 0; i < 2; i++) {
-    console.log(Tool.memUsage(`第 ${i} 次读取前`));
-    readFileSync(join(ROOT, '/temp/largefile.zip'), 'utf-8');
-    console.log(Tool.memUsage(`第 ${i} 次读取后`));
-}
